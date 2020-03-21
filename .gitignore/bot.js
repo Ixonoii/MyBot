f6c8c70143dcc4660469c5818d8858b503093468
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-03-21 23:40:11 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -759,7 +759,7 @@ client.on('message', function (message) {
         if(!message.guild.name == "MyBot Support Server") return
         if(!message.member.roles.some(r=>["Founder | Developer","."].includes(r.name)) ) return message.channel.send(NotAllowed)
         let IDEntered = parseInt(args[1])
-        let MessageEntered = args.slice(2).join(" ")
+        let MessageEntered = args.slice(3).join(" ")
         if (!IDEntered) return message.channel.send(NoIDEntered)
         if (isNaN(IDEntered)) return message.channel.send(NoIDEntered)
         if (!MessageEntered) return message.channel.send(NoMessageEntered)
@@ -769,6 +769,6 @@ client.on('message', function (message) {
         .setDescription(MessageEntered)
         .setTimestamp()
         .setFooter("Sent by " + message.author.tag + ".")
-        client.users.get(IDEntered).send(MessageEmbed)
+        client.users.get(IDEntered).send(MessageEntered)
     }
 })
