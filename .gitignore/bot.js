69096455f6ed465218319742832a3951a3d3185d
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-04-14 18:41:36 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -795,3 +795,34 @@ client.on('message', function (message) {
         message.channel.send(Success)
     }
 })
+
+client.on("message", message =>{
+    let invites = ["ignore me"], ct = 0;
+    client.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites =>{
+            invites[invites.length + 1] = (g + " - `Invites: " + guildInvites.array().join(", ") + "`");
+            ct++;
+
+            if(ct >= client.guilds.size) {
+                invites.forEach((invite, i) => {
+                    if(invite == undefined)
+                    invites.splice(i, 1);
+
+                    invites.shift();
+                    invites.forEach((invite, i) => invites[i] = "- " + invite);
+                    invites = invites.join("\n\n");
+
+                    let embed = new Discord.RichEmbed()
+                    .setTitle("All Invites:")
+                    .setDescription(invites);
+
+                    message.channel.send(embed);
+
+                    if(message.content === prefix + "--invites") {
+                        message.channel.send(embed)
+                    }
+                })
+            }
+        })
+    })
+})
