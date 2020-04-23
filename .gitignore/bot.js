d27348bb711fbc54d3d5ba2bcac8872030b5c515
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-04-23 18:36:00 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,7 +11,7 @@ function emoji (id) {
 }
 
 client.on('ready', function(){
-    client.user.setActivity("🎉 MyBot is back online, sorry for the inconvenience. 😀", {type: "PLAYING"})
+    client.user.setActivity("😷 Stay home, stay safe! 🏡", {type: "PLAYING"})
 })
 
 // CMDS COMMAND
